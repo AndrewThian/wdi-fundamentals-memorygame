775c9e2cb0cd399f54b26717517833dce3adb20c
AndrewThian <andrewbobbythian@gmail.com>
2016-12-10 20:05:14 +0800

    first pass

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,13 @@
-console.log("JS file is connected to HTML! Woo!")
+// alert('Hello, friends.');
+//
+// if (div.card1 && div.card3) {
+//   prompt("that's right")
+// } else if (div.card2 && div.card4) {
+//   prompt("that's right")
+// } else {
+//   prompt('try again!')
+// }
+//
+// function myFunction() {
+//   alert("I am an alert box!");
+// }
